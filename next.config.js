b9a95511dbfa6433ed09b32700ee7465feab1978
RumbleKAT <reki318@naver.com>
2021-12-27 22:54:11 +0900

    Redirect & Rewrite
    Set Private API_KEY .env

--- next.config.js
+++ next.config.js
@@ -1,3 +1,27 @@
+const API_KEY = process.env.API_KEY
+
 module.exports = {
   reactStrictMode: true,
+  async redirects(){
+    return [
+      {
+        source : "/contact",
+        destination : "/form",
+        permanent : false
+      }
+    ]
+  },
+  async rewrites(){
+    return [
+      {
+        source: "/api/movies",
+        destination : `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+      }
+    ]
+  }
 }
+/*
+  api key를 숨기기 위한 조치 => redirect
+  설정 후 재시작
+  source : old-blog/:path* => 뒤에 패턴 매칭하면 모두 리다이렉트시킨다.
+*/
